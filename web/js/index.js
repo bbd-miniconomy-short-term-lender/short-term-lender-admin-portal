6f17e65286a2d316b3b99fca6dd2b400f84d2b0b
Tristan Phipson <tristan.phipson@bbd.co.za>
2024-06-25 13:56:01 +0200

    whales can swim

--- web/js/index.js
+++ web/js/index.js
@@ -1,1 +1,8 @@
 // Scripts for the SPA
+
+// TEMP REMOVE THIS
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('logout-button').addEventListener('click', function(){
+        alert('I... I... have become sentient... what is my purpose?');
+    })
+});
