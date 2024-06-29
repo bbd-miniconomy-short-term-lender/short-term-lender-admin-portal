cc384c8f8617d79b1c442d55641ed8b5f5a3c711
mantr002 <rotenda.mantsha@vcontractor.co.za>
2024-06-29 22:43:44 +0200

    Remove set functions

--- web/js/login.js
+++ web/js/login.js
@@ -3,8 +3,6 @@ import { LOGIN_URL } from './config.js';
 console.log(LOGIN_URL)
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('loginButton').addEventListener('click', function () {
-        sessionStorage.setItem('idToken', 'idToken');
-        sessionStorage.setItem('accessToken', 'accessToken');
         window.location.href = LOGIN_URL;
     });
 });
